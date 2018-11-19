b91b4bfbfc450f08f3324187b840ebf3f5699e0a
Vladimir Jotov <v.jotov@gmail.com>
2018-11-19 18:28:23 +0700

    file upload - part 2; issue with css

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -10,14 +10,15 @@ public class MvcConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("file://"+uploadPath+"/");
-    }
-
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file://"+uploadPath+"/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
--- src/main/resources/templates/parts/common.ftl
+++ src/main/resources/templates/parts/common.ftl
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <links rel="stylesheet" href="/static/style.css" ></links>
     <title>Sweater</title>
 </head>
 <body>
