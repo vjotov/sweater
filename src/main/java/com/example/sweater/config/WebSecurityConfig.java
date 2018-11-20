6ab88a7f8e087e11b42a9588c821b3d2ad3f6513
Vladimir Jotov <v.jotov@gmail.com>
2018-11-21 00:10:59 +0700

    adding emails and activation code logic (not tested)

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**").permitAll()
+                    .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
