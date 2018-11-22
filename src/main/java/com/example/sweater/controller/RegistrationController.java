217eb59e90ee353e3f53ac52b6cae5ecbc4b01d3
Vladimir Jotov <v.jotov@gmail.com>
2018-11-22 23:43:43 +0700

    bean validation - part 3

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -24,11 +26,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult,Model model) {
-        if( user.getPassword() != null && !user.getPassword().equals(user.getPassword2())){
+    public String addUser(
+            @RequestParam("password2") String passwordConfirmation,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirmation);
+        if(isConfirmEmpty){
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+        if( user.getPassword() != null && !user.getPassword().equals(passwordConfirmation)){
             model.addAttribute("passwordError", "Passwords are different!");
         }
-        if(bindingResult.hasErrors()){
+        if(isConfirmEmpty || bindingResult.hasErrors()){
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
             return "registration";
@@ -45,8 +56,11 @@ public class RegistrationController {
         boolean isActivated = userService.activateUser(code);
 
         if(isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated.");
+
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found!");
         }
 
